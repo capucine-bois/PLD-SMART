0b1fb9e913ea01313f1233427e700d3f7c425ef0
Louis <louistricotkarl@gmail.com>
2022-04-12 11:26:51 +0200

    Ajout de l'ossature du projet

---
+++ src/backend/java/com/backend/heka/HekaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.heka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/heka/HekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.heka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
