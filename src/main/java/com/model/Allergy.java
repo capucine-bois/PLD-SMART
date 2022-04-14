da82d39c7d7639d008fe0076c963c29326ab30e2
plvernay <pierre-louis.vernay@insa-lyon.fr>
2022-04-14 14:32:31 +0200

    add getters and setters

--- src/main/java/com/model/Allergy.java
+++ src/main/java/com/model/Allergy.java
@@ -25,4 +25,52 @@ public class Allergy {
     @Column(name = "id_medical_file",nullable = false)
     private MedicalFile medicalFile;
 
+    public Allergy(long allergyId, String name, short severity, MedicalFile medicalFile) {
+        this.allergyId = allergyId;
+        this.name = name;
+        this.severity = severity;
+        this.medicalFile = medicalFile;
+    }
+
+    public long getAllergyId() {
+        return allergyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public short getSeverity() {
+        return severity;
+    }
+
+    public MedicalFile getMedicalFile() {
+        return medicalFile;
+    }
+
+    public void setAllergyId(long allergyId) {
+        this.allergyId = allergyId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSeverity(short severity) {
+        this.severity = severity;
+    }
+
+    public void setMedicalFile(MedicalFile medicalFile) {
+        this.medicalFile = medicalFile;
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Allergy{" +
+                "allergyId=" + allergyId +
+                ", name='" + name + '\'' +
+                ", severity=" + severity +
+                ", medicalFile=" + medicalFile +
+                '}';
+    }
 }
