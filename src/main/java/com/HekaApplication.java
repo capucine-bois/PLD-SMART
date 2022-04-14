8078849263fec834a23073eafc29b745497dc876
Louis <louistricotkarl@gmail.com>
2022-04-14 16:56:37 +0200

    "Wola ca marche je suis si heureux "

--- src/main/java/com/HekaApplication.java
+++ src/main/java/com/HekaApplication.java
@@ -1,7 +1,11 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HekaApplication {
@@ -10,4 +14,14 @@ public class HekaApplication {
 		SpringApplication.run(HekaApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:19006").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+			}
+		};
+	}
 }
