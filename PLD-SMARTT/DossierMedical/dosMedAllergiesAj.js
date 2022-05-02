83c80c703ae3df91e39950d157126ace32507523
aur-inard <aurelia.inard@hotmail.fr>
2022-05-02 09:01:41 +0200

    Revert "Merge remote-tracking branch 'origin/front' into front"
    This reverts commit b083ba0be5fa5d41850b054789cc06e93b56e2c3, reversing
    changes made to fb75e253fab5694864958575e89601e05176bcda.

--- PLD-SMARTT/DossierMedical/dosMedAllergiesAj.js
+++ PLD-SMARTT/DossierMedical/dosMedAllergiesAj.js
@@ -2,17 +2,6 @@ import {React,useState} from 'react';
 import {StyleSheet, Text, ScrollView, View, TouchableOpacity, TextInput} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {StatusBar} from "expo-status-bar";
-import {Picker} from "@react-native-picker/picker"
-
-function Bouton(props){
-    return (
-        <TouchableOpacity style={props.styleButton} onPress={props.onPress}>
-            <Text style={props.styleText}>
-                {props.text}
-            </Text>
-        </TouchableOpacity>
-    )
-}
 
 function DosMedAllergiesAj({navigation}) {
     const prenom = "Gérard"
@@ -20,16 +9,9 @@ function DosMedAllergiesAj({navigation}) {
     const[titre,setTitre]=useState('');
     const[severite,setSeverite]=useState('');
     const[note,setNote]=useState('');
-    const [selectedValue, setSelectedValue] = useState("type");
+
     const[ajouterModifier,setAjouterModifier]=useState('Ajouter');
     const [bouton, setBouton] = useState(false);
-    const data = [{
-        value: 'Banana',
-    }, {
-        value: 'Mango',
-    }, {
-        value: 'Pear',
-    }];
     return(
         <View style={styles.container}>
             <View style={styles.headerBtn}>
@@ -43,58 +25,60 @@ function DosMedAllergiesAj({navigation}) {
                     })}/>
                 </TouchableOpacity>
             </View>
-            <StatusBar style="auto" />
             <View style = {styles.titre}>
                 <Text style={styles.text}>
                     Nouvelle allergie
                 </Text>
             </View>
-
             <View style={styles.inputView}>
-                <Text style={styles.text3}>
-                    Titre
-                </Text>
+                <StatusBar style="auto" />
                 <TextInput
                     style={styles.TextInput}
+                    placeholder="Titre"
                     placeholderTextColor="#000"
                     onChangeText={(titre) => setTitre(titre)}
                 />
             </View>
 
             <View style={styles.inputView}>
-                <Text style={styles.text3}>
-                    Type
-                </Text>
-                <Picker
-                    selectedValue={selectedValue}
-                    style={styles.picker}
-                    onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
-                >
-                    <Picker.Item label="Alimentaire" value="alim" />
-                    <Picker.Item label="Saisonnière" value="saison" />
-                    <Picker.Item label="Perannuelle" value="peran" />
-                    <Picker.Item label="Au venin" value="venin" />
-                </Picker>
-            </View>
-            <View style={styles.descriptif}>
-                <Text style={styles.text4}>
-                    Descriptif
-                </Text>
-                <ScrollView style={styles.scrollView}>
-                    <TextInput
-                        style={styles.TextInput2}
-                        multiline={true}
-                        placeholderTextColor="#000"
-                        onChangeText={(note) => setNote(note)}
-                    />
-                </ScrollView>
-            </View>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Séverité"
+                    placeholderTextColor="#000"
+                    onChangeText={(severite) => setSeverite(severite)}
+                />
 
-            <View style={{height:"15%", marginHorizontal:"15%", flexDirection:"row", justifyContent:"space-between"}}>
-                <Bouton styleButton={styles.btnAjout} styleText={styles.text2} onPress={() =>  navigation.navigate('DosMedAllergies', {
-                })} text="Ajouter"/>
-                <Bouton styleButton={styles.btnAnnuler} styleText={styles.text2} onPress={() =>  navigation.navigate('DosMedAllergies', {
-                })} text="Annuler"/>
+            </View>
+            <Text style={{textAlign:'center',marginTop:40, color:"#1EA584",fontSize: 25, fontWeight: 'bold'}}>
+                Descriptif
+            </Text>
+            <ScrollView style={{marginTop:20,textAlign:'left',width: "80%",height:"40%",borderWidth: 5,borderColor:"#1EA584",borderRadius:15, alignSelf:"center"}}>
+                <TextInput
+                    style={styles.TextInput}
+                    placeholder="Décrire mon allergie"
+                    placeholderTextColor="#000"
+                    onChangeText={(note) => setNote(note)}
+                />
+
+            </ScrollView>
+
+            <View style={{height:"15%"}}>
+
+
+                <TouchableOpacity style={styles.ajoute}  >
+                    <Text style={styles.text}>
+                        Ajouter
+                    </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.annule} onPress={()=>navigation.navigate('DosMedAllergies', {
+                    prenom: prenom,
+                    nom: nom,
+                })}>
+                    <Text style={styles.text}>
+                        Annuler
+                    </Text>
+                </TouchableOpacity>
 
             </View>
 
@@ -106,66 +90,14 @@ function DosMedAllergiesAj({navigation}) {
 export default DosMedAllergiesAj
 
 const styles = StyleSheet.create({
-    iconDossier: {
-        marginRight:"5%"
-    },
-    descriptif:{
-        flexDirection:"column",
-        width: "80%",
-        height:"40%",
-        marginTop:"10%",
-        alignSelf:"center",
-    },
-    scrollView:{
-        borderWidth: 5,
-        borderColor:"#1EA584",
-        borderRadius:15,
-    },
-    text:{
+    text: {
         fontSize: 25,
         fontWeight: 'bold',
-        color: "#1EA584",
+        color: "#fff",
         alignSelf:"center"
     },
-    picker:{
-        backgroundColor: "#B8E6DA",
-        width: "60%",
-        paddingHorizontal:"5%",
-        color: "#000000",
-        alignSelf:"center"
-    },
-    text3:{
-        color:"#1EA584",
-        fontSize: 22,
-        fontWeight: 'bold',
-        alignSelf:"center"
-    },
-    btnAjout: {
-        width: "45%",
-        flexDirection:"row",
-        borderRadius: 25,
-        height: "45%",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        backgroundColor: "#1EA584",
-        marginTop:"10%"
-    },
-    btnAnnuler: {
-        width: "45%",
-        flexDirection:"row",
-        borderRadius: 25,
-        height: "45%",
-        alignItems: "center",
-        justifyContent: "space-evenly",
-        backgroundColor: "#695353",
-        marginTop:"10%"
-    },
-    text4:{
-        color:"#1EA584",
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop:"5%",
-        marginBottom:"2%"
+    iconDossier: {
+        marginRight:"5%"
     },
     headerBtn: {
         width: "100%",
@@ -175,6 +107,7 @@ const styles = StyleSheet.create({
         alignItems:"flex-end",
         paddingBottom:20,
         backgroundColor: "#1EA584"
+
     },
     container: {
         backgroundColor: '#ffffff'
@@ -187,32 +120,26 @@ const styles = StyleSheet.create({
         flex: 1
     },
     TextInput: {
-        backgroundColor: "#B8E6DA",
-        borderRadius: 30,
-        width: "60%",
-        height: "100%",
-        textAlign:"center",
-        paddingHorizontal:"5%",
+        height: 40,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
         color: "#000000",
-        alignSelf:"center"
-    },
-    TextInput2: {
-        width: "100%",
-        padding:"5%",
-        color: "#000000",
-        fontSize:17
     },
     inputView: {
-        flexDirection:"row",
-        justifyContent:"space-between",
-        paddingHorizontal:"10%",
-        marginTop:"5%"
+        alignSelf:"center",
+        marginTop: 10,
+        flexDirection: 'row',
+        backgroundColor: "#B8E6DA",
+        borderRadius: 30,
+        width: "80%",
+        height: 50,
+        alignItems: "center",
     },
     titre:{
         alignSelf:"center",
-        margin:"10%",
-        color:"#000",
-
+        margin:"5%",
+        color:"#000"
     },
 })
 
