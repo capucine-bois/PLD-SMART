39929380165c1b82e7a9d1f276256fcea9495e8d
plvernay <pierre-louis.vernay@insa-lyon.fr>
2022-05-04 10:01:47 +0200

    Update UserNotFoundException.java

--- src/main/java/com/controller/UserNotFoundException.java
+++ src/main/java/com/controller/UserNotFoundException.java
@@ -4,6 +4,6 @@ import com.model.User;
 public class UserNotFoundException extends RuntimeException {
 
     UserNotFoundException(String token){
-        super("Could not find user " + token);
+        super("Could not find user with token " + token);
     }
 }
